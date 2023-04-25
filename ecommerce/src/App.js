5f89faca99aecad60f6741ac299e8301865d9b45
julianrodriguez2728 <julirdrg@gmail.com>
2023-04-25 19:06:34 -0300

    NAVBAR JR

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,30 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      <div>
-        <p>Hi, i´m The Guiso</p>
-        <p>Hi, i´m the juleon27</p>
-        <p>hi there, i'm using whatsapp</p>
-        <p>anda?</p>
-      </div>
+    <div>
+      <NavBar/>
     </div>
   );
 }
---
+++ ecommerce/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,27 @@
+import "./navbar.css";
+import React from "react";
+import img from "./Diseno_sin_titulo-removebg-preview.png";
+import { FaCartPlus , FaSearch, FaUser } from "react-icons/fa";
+
+
+const NavBar = ()=>{
+    return(
+        <nav>
+            <div className="nav-up">
+                <img src={img} alt="" className="logo-nav" />
+                <div className="search-container">
+                <input type="text"  className="searchBar" placeholder="busca tu producto..."/>
+                <button className="btn-search">
+                <  FaSearch style={{color: "white"}} size={"20px"}/> 
+                </button>
+                </div>
+                <div>
+                < FaCartPlus style={{color: "white"}} size={"45px"} className="cart"/> 
+                < FaUser style={{color: "white"}} size={"45px"} className="user"/> 
+                </div>
+            </div>
+        </nav>
+    )
+}
+ 
+export default NavBar;
